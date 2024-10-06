f5843cf18c91ca662d7b682d4965cc56d8349d62
Conar365 <kvazar382@gmail.com>
2024-10-06 23:22:41 +0300

    scss

--- compilete_dev.py
+++ compilete_dev.py
@@ -7,3 +7,7 @@ if int(let) == 0:
     name = input("Name: ")
     os.system(f'git commit -m "{name}"')
     os.system("git push")
+elif int(let) == 1:
+    os.system("sass prefab/main.scss static/css/main.css")
+    os.system("sass prefab/w.scss static/css/w.css")
+    os.system("sass prefab/b.scss static/css/b.css")
